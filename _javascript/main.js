5f8ebe7e65be0a8082625a6e71dd438e128fd9c2
DaliborA <d.arula@adriamedia.hr>
2022-03-25 11:54:35 +0100

    fix i korekcije
    korekcije i animacija fix

--- _javascript/main.js
+++ _javascript/main.js
@@ -11,7 +11,7 @@ gsap.from('.header__title', {
   }
 })
 
-if (document.querySelector('body').offsetWidth > 1024) {
+if (document.querySelector('body').offsetWidth > 1500) {
   // Picture left
   var picturesLeft = document.querySelectorAll('.picture__left');
   var slideLeft = gsap.timeline({});
@@ -21,10 +21,10 @@ if (document.querySelector('body').offsetWidth > 1024) {
           y: 1900,
           ease: 'none',
           scrollTrigger: {
-              trigger: pic,
+              trigger: pic.parentNode,
               scrub: true,
-              start: 'top 30%',
-              end: 'bottom top'
+              start: 'top 50%',
+              end: 'bottom 70%'
           }
       })
   });
@@ -42,7 +42,77 @@ if (document.querySelector('body').offsetWidth > 1024) {
               trigger: pic_r.parentNode,
               scrub: true,
               start: 'top 50%',
-              end: 'top top'
+              end: 'bottom 70%'
+          }
+      })
+  });
+} else if (document.querySelector('body').offsetWidth > 1024) {
+  // Picture left
+  var picturesLeft = document.querySelectorAll('.picture__left');
+  var slideLeft = gsap.timeline({});
+  
+  picturesLeft.forEach(pic => {
+      slideLeft.to(pic, {
+          y: 1800,
+          ease: 'none',
+          scrollTrigger: {
+              trigger: pic.parentNode,
+              scrub: true,
+              start: 'top 30%',
+              end: 'bottom 70%'
+          }
+      })
+  });
+  
+  // Picture right
+  var picturesRight = document.querySelectorAll('.picture__right');
+  var slideRight = gsap.timeline({});
+  
+  picturesRight.forEach(pic_r => {
+      console.log(pic_r.parentNode)
+      slideRight.to(pic_r, {
+          y: -1050,
+          ease: 'none',
+          scrollTrigger: {
+              trigger: pic_r.parentNode,
+              scrub: true,
+              start: 'top 30%',
+              end: 'bottom 70%'
+          }
+      })
+  });
+} else {
+  // Picture left
+  var picturesLeft = document.querySelectorAll('.picture__left');
+  var slideLeft = gsap.timeline({});
+  
+  picturesLeft.forEach(pic => {
+      slideLeft.from(pic, {
+          y: -500,
+          ease: 'none',
+          scrollTrigger: {
+              trigger: pic.parentNode,
+              scrub: true,
+              start: 'top 70%',
+              end: 'bottom 70%'
+          }
+      })
+  });
+  
+  // Picture right
+  var picturesRight = document.querySelectorAll('.picture__right');
+  var slideRight = gsap.timeline({});
+  
+  picturesRight.forEach(pic_r => {
+      console.log(pic_r.parentNode)
+      slideRight.from(pic_r, {
+          y: 500,
+          ease: 'none',
+          scrollTrigger: {
+              trigger: pic_r.parentNode,
+              scrub: true,
+              start: 'top 70%',
+              end: 'bottom 70%'
           }
       })
   });
